e187f20071cd0dcfaf259edb8c808d786574c363
Markus Weinberger <markus.weinberger@stud.uni-regensburg.de>
2026-02-09 19:05:20 +0100

    Branch angelegt und die nicht merh benötigten imports bereinigt

--- app/src/main/java/com/example/carbooking/BookingsActivity.kt
+++ app/src/main/java/com/example/carbooking/BookingsActivity.kt
@@ -2,20 +2,9 @@ package com.example.carbooking
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.carbooking.ui.theme.CarBookingTheme
 
 class BookingsActivity : AppCompatActivity() {
 
